073a9dec6149074857095c75678c2a4a2b8c6040
azotamiota <azotamiota@gmail.com>
2022-10-20 11:18:59 +0100

    setup basic server & database

---
+++ controllers/user.js
@@ -0,0 +1,2 @@
+MONGO_URI="mongodb+srv://aggie-norbert:fp_Graduates2022@cluster0.ejb0ecs.mongodb.net/?retryWrites=true&w=majority"
+PORT=3000
---
+++ index.js
@@ -0,0 +1,17 @@
+const app = require('./app')
+const connectDB = require('./db/connect')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const port = process.env.PORT || 5000
+
+const connectDatabase = async () => {
+    
+        await connectDB(process.env.MONGO_URI)
+            .then(() => console.log('Connected to database...'))
+            .then(() => app.listen(port))
+            .then(() => console.log(`Server is running on port ${port}`))
+            .catch(error => console.log('Can\'t connect to db or server: ', error))
+            
+}
+connectDatabase()
